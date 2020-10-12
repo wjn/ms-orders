4c8f639092c87684093b0c6f96db0225c56cb688
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-10-12 07:02:41 -0500

    added ticket and order models

--- src/models/ticket.ts
+++ src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Order, OrderStatus } from './order';
 
 interface TicketAttrs {
   title: string;
@@ -8,6 +9,7 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -38,6 +40,42 @@ const ticketSchema = new mongoose.Schema(
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
+};
+
+/**
+ * To be reserved, a ticket must be associated with an order which
+ * has a status that is anything but 'canceled'.
+ * We'll need to search all orders to answer this question. If we do
+ * find an order, it means that the ticket *IS* reserved.
+ */
+
+ticketSchema.methods.isReserved = async function (): Promise<boolean> {
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  });
+
+  /** Use of the Double Bang (!!).
+   *  -------------------------------------------------------------------
+   *  if an order is found existingOrder (eo) is truthy.
+   *  !eo = false
+   *  !!eo = true
+   *
+   *  if an order is NOT found eo is falsey
+   *  !eo = true
+   *  !!eo = false
+   *
+   *  so the use of the double bang (!!) essentially converts something
+   *  from truthy/falsey to a straightup boolean of true/false.
+   *  ------------------------------------------------------------------- */
+
+  return !!existingOrder;
 };
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
