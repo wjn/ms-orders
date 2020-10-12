9f28714073e59981e7bd6d098a2cd83fd0ce0d82
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-10-12 07:04:04 -0500

    updated new route to create ticket (untested)

--- src/routes/new.ts
+++ src/routes/new.ts
@@ -1,13 +1,23 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@nielsendigital/ms-common';
+import {
+  OrderStatus,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+  BadRequestError,
+} from '@nielsendigital/ms-common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
 router.post(
   '/api/orders',
-  requireAuth,
+  requireAuth, // << currentUser is guaranteed to be defined.
   [
     body('ticketId')
       .not()
@@ -18,8 +28,42 @@ router.post(
       })
       .withMessage('TicketId must be provided'),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    res.send({});
+    const { ticketId } = req.body;
+
+    // 1. find ticket user wants to order in the DB or return error
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+      throw new NotFoundError('Ticket ID not found');
+    }
+
+    // 2. insure that ticket isn't already reserved
+    if (await ticket.isReserved()) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // 3. calculate an expiration date for the order
+    const expirationDate = new Date();
+    expirationDate.setSeconds(
+      expirationDate.getSeconds() + EXPIRATION_WINDOW_SECONDS
+    );
+
+    // 4. build the order and save to the database
+    // 4.a. Build up the order in mongo
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expirationDate,
+      ticket: ticket,
+    });
+
+    // 4.b. save to database.
+    await order.save();
+
+    // 5. publish order:created event
+    res.status(201).send(order);
   }
 );
 
